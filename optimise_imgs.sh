8409df671e5841af63ce6c3f2daf937799bf2a45
ISSOtm <me@eldred.fr>
2024-12-21 12:43:56 +0100

    Improve how images are optimised

--- optimise_imgs.sh
+++ optimise_imgs.sh
@@ -3,22 +3,6 @@ set -uo pipefail
 
 git ls-files '*.svg' | xargs -I '{}' svgo --multipass -i'{}'
 
-git ls-files '*.png' '*.jpg' | while read -r file; do
-	orig=$(wc -c <"$file")
-	if [[ "$file" = *.jpg ]]; then
-		convert "$file" "${file%.jpg}.png"
-	fi
-	oxipng -Zaso max "${file%.*}.png"
-	new=$(wc -c <"${file%.*}.png")
-	printf '%s: %u → %u\n' "$file" "$orig" "$new"
-	if [[ "$file" = *.jpg ]]; then
-		if [[ "$new" -lt "$orig" ]]; then
-			git rm "$file"
-			git add "${file%.jpg}.png"
-		else
-			rm "${file%.jpg}.png"
-		fi
-	elif [[ "$new" -gt "$orig" ]]; then
-		git restore "$file"
-	fi
-done
+git ls-files '*.png' | xargs oxipng -Zaso max
+
+git ls-files '*.jpg' '*.jpeg' | xargs jpegoptim --strip-com

bb79750943a06448433ab11775458d122dc66d29
ISSOtm <me@eldred.fr>
2024-12-22 23:47:22 +0100

    Reduce quality of JPEG images slightly
    85 is honestly okay

--- optimise_imgs.sh
+++ optimise_imgs.sh
@@ -5,4 +5,4 @@ git ls-files '*.svg' | xargs -I '{}' svgo --multipass -i'{}'
 
 git ls-files '*.png' | xargs oxipng -Zaso max
 
-git ls-files '*.jpg' '*.jpeg' | xargs jpegoptim --strip-com
+git ls-files '*.jpg' '*.jpeg' | xargs jpegoptim --strip-com -m85
